2d3d408b569021352fa324b5507174bf5b4cec4c
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-03-07 19:20:22 -0600

    Added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,29 +3,29 @@ $(document).ready(function() {
     // Create an array of characters with their starting stats
     var characters = [{
         name: "Luke Skywalker",
-        img: "https://place-hold.it/200x150",
-        card: "",
+        img: "assets/images/Luke_Skywalker.png",
         hp: 1000,
         baseAtk: 8,
         counterAtk: 25
     },
     {
         name: "Obi-Wan Kenobi",
-        img: "https://place-hold.it/200x150",
+        img: "assets/images/Obi-Wan_Kenobi.png",
+
         hp: 120,
         baseAtk: 8,
         counterAtk: 25
     },
     {
         name: "Darth Vader",
-        img: "https://place-hold.it/200x150",
+        img: "assets/images/Darth_Vader.png",
         hp: 180,
         baseAtk: 8,
         counterAtk: 25
     },
     {
         name: "Emperor Palpatine",
-        img: "https://place-hold.it/200x150",
+        img: "assets/images/Emperor_Palpatine.png",
         hp: 150,
         baseAtk: 8,
         counterAtk: 25
@@ -37,6 +37,13 @@ $(document).ready(function() {
     var enemySelected = false;
     var gameOver = false;
 
+    // Hide all except for character selection
+    $("#select-character-card").show();
+    $("#your-character-card").hide();
+    $("#current-enemy-card").hide();
+    $("#remaining-enemies-card").hide();
+    $("#attack-btn").hide();
+    
     // Create a card for each character and add them to the "Select Your Character" card
     //characters.forEach(fuction(theCharacter) {
     $.each(characters, function(i, theCharacter) {
@@ -44,7 +51,6 @@ $(document).ready(function() {
         characters[i].card = $("<div>");
         characters[i].card.addClass("card character-card");
         characters[i].card.attr("index", i);
-        characters[i].card.css("display", "inline-block")
 
         // Add the character image
         var img = $("<img>");
@@ -91,6 +97,11 @@ $(document).ready(function() {
 
                 // Move the other cards to "Remaining Opponents"
                 $("#remaining-enemies-card > .card-body").append($("#select-character-card > .card-body > .character-card"));
+
+                // Hide and show cards
+                $("#select-character-card").hide();
+                $("#your-character-card").show();
+                $("#remaining-enemies-card").show();
             } else if (!enemySelected) {
                 // Check that enemy card was clicked
                 if($(this).attr("index") !== iPlayer) {
@@ -101,6 +112,10 @@ $(document).ready(function() {
 
                     // Move the selected card to "Selected Opponent"
                     $("#current-enemy-card > .card-body").append(this);
+
+                    // Show attack button and enemy
+                    $("#current-enemy-card").show();
+                    $("#attack-btn").show();
                 }
             }
         }
@@ -109,6 +124,9 @@ $(document).ready(function() {
     $("#attack-btn").on("click", function() {
         // Player attacks enemy
         characters[iEnemy].hp -= currentAtk;
+
+        // Update text
+        $("#attack-txt").text("You attacked for " + currentAtk + " damage.");
 
         // Check if enemy is dead
         if (characters[iEnemy].hp <= 0) {
@@ -125,28 +143,29 @@ $(document).ready(function() {
             if ($("#remaining-enemies-card > .card-body").children().length <= 0) {
                 endGame(true);
             }
-            
-            return
+        } else {
+            // Update HP text
+            $(characters[iEnemy].card).find(".hp-text").text(characters[iEnemy].hp);
+
+            // Enemy attacks player
+            characters[iPlayer].hp -= characters[iEnemy].counterAtk;
+
+            // Update text
+            $("#counter-attack-txt").text(characters[iEnemy].name + " attacked you for " + characters[iEnemy].counterAtk + " damage.");
+
+            // Check if player is dead
+            if (characters[iPlayer].hp <= 0) {
+                // Ensure negative HP is not displayed
+                characters[iPlayer].hp = 0;
+
+                // Player loses
+                endGame(false);
+            }
+
+            // Update HP text
+            $(characters[iPlayer].card).find(".hp-text").text(characters[iPlayer].hp);
         }
-        // Update HP text
-        $(characters[iEnemy].card).find(".hp-text").text(characters[iEnemy].hp);
-
-        // Enemy attacks player
-        characters[iPlayer].hp -= characters[iEnemy].counterAtk;
-
-        // Check if player is dead
-        if (characters[iPlayer].hp <= 0) {
-            // Ensure negative HP is not displayed
-            characters[iPlayer].hp = 0;
-
-            // Player loses
-            endGame(false);
-        }
-
-        // Update HP text
-        $(characters[iPlayer].card).find(".hp-text").text(characters[iPlayer].hp);
-
-
+        
         // Increase player attack power
         currentAtk += characters[iPlayer].baseAtk;
     })
